258fed1a0e085e2de700ecd97d3b265f2e5420c5
Jagman Sidhu <jagmansidhu1@icloud.com>
2026-06-25 08:41:07 -0700

    Add: minimum-window-with-characters - submission-1

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-1.py
@@ -0,0 +1,40 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if t == "":
+            return ""
+
+        count = {}
+        res = ""
+
+        count_cur = 0
+        min_len = float("inf")
+
+        for i in t:
+            count[i] = count.get(i, 0) + 1
+
+        l = 0
+        for r, char in enumerate(s):
+            # here if the item is in char and == 0 we increment our count
+            if char in count:
+                count[char] = count.get(char) - 1
+                if count[char] == 0:
+                    count_cur += 1
+
+            # once our count is at the length of our t
+            # we will first check to see if our window string is smaller than current string
+            # if smaller res = window
+            # We will also update l positions place in the count and increment +1
+            while count_cur == len(count):
+
+                if (r - l + 1) < min_len:
+                    min_len = r - l + 1
+                    res = s[l:r+1] 
+
+                if s[l] in count:
+                    if count[s[l]] == 0:
+                        count_cur -= 1
+                    count[s[l]] += 1
+                
+                l += 1
+
+        return res
